fc7ec5c7edb878eac0c545a170a7e13313a33801
RoshanCS <roshan.cs790@gmail.com>
2020-11-25 22:01:01 +0530

    some lines added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
 using namespace std;
+
+int b[6] = {1, 7, 3, 4, 5};
+int* bptr = &b[0];
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    int a = 23;
+    int* aptr;
+
+    //cout << sizeof(b[0])<< endl;
+    cout<< *bptr <<endl;
+
     return 0;
 }
